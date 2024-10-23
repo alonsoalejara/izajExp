d35b7228df5b0993db19f3f7d391b9f5cecc60eb
Alonso Alé-Jara <alonsoignacioalejara@gmail.com>
2024-10-23 20:16:07 -0300

    feat: inicialización de backend

--- izaj-ei/App.js
+++ izaj-ei/App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 import icon from './assets/EI-Montajes.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={icon} style={{ width: 305, height: 130 }} />
-      <Text>App de Plan de Izaje.</Text>
-      <StatusBar style="auto" />
+      <Image source={icon} style={{
+        width: 200, 
+        height: 100,
+        resizeMode: 'contain',
+        }} />
+      <Text style={{color: 'white'}}>App de Plan de Izaje.</Text>
+      <StatusBar style="light" />
+      <Button 
+        title="Pulsa aquí" 
+        onPress = {() => alert('Hola amigos!')} 
+      />
     </View>
   );
 }
@@ -16,7 +24,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
