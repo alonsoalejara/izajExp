7a6c38c2636510a527087cf94de27bddb6895f6e
Alonso Alé-Jara <67810324+alonsoalejara@users.noreply.github.com>
2024-11-18 22:20:59 -0300

    Delete izaj-ei directory

--- izaj-ei/server/api/routes/maniobra.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { crearManiobra } from '../controller/maniobra.controller.js';
-
-const router = Router();
-
-// Ruta para crear una nueva maniobra
-router.post('/', crearManiobra);
-
-export default router;
