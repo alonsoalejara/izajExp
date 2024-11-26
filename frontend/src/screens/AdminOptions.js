e860f8c7fdfe922861d3275d7fad465206f85519
Alonso Alé-Jara <alonsoignacioalejara@gmail.com>
2024-11-25 22:33:32 -0300

    style: Correcciones de logos y diseños

--- frontend/src/screens/AdminOptions.js
+++ frontend/src/screens/AdminOptions.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, TouchableOpacity, ImageBackground, Image } from "react-native";
 import Svg, { LinearGradient, Stop, Rect } from "react-native-svg";
-import LoginStyles from "../styles/LoginStyles";
+import AdminOptionsStyles from "../styles/AdminOptionsStyles";
 
 export default function AdminOptions({ navigation }) {
   const handleRoleSelection = (role) => {
@@ -14,35 +14,35 @@ export default function AdminOptions({ navigation }) {
   };
 
   return (
-    <View style={LoginStyles.container}>
+    <View style={AdminOptionsStyles.container}>
       {/* Interfaz de usuario */}
-      <View style={LoginStyles.circleContainer}>
+      <View style={AdminOptionsStyles.circleContainer}>
         <ImageBackground
           source={require("../../assets/grua-home.png")}
-          style={LoginStyles.background}
-          imageStyle={LoginStyles.image}
+          style={AdminOptionsStyles.background}
+          imageStyle={AdminOptionsStyles.image}
         >
-          <Svg style={LoginStyles.gradient}>
+          <Svg style={AdminOptionsStyles.gradient}>
             <LinearGradient id="grad1" x1="0%" y1="0%" x2="0%" y2="100%">
               <Stop offset="10%" stopColor="white" stopOpacity="0.6" />
               <Stop offset="90%" stopColor="red" stopOpacity="0.8" />
             </LinearGradient>
             <Rect width="100%" height="100%" fill="url(#grad1)" />
           </Svg>
-          <Image source={require("../../assets/EI-Montajes.png")} style={LoginStyles.logo} resizeMode="contain" />
+          <Image source={require("../../assets/EI-Montajes.png")} style={AdminOptionsStyles.logo} resizeMode="contain" />
         </ImageBackground>
       </View>
 
-      <View style={LoginStyles.formContainer}>
-        <Text style={LoginStyles.title}>Seleccione el rol que desea utilizar</Text>
+      <View style={AdminOptionsStyles.formContainer}>
+        <Text style={AdminOptionsStyles.title}>Seleccione el rol que desea utilizar</Text>
         
         {/* Botones de selección de rol */}
-        <TouchableOpacity style={LoginStyles.button} onPress={() => handleRoleSelection("admin")}>
-          <Text style={LoginStyles.buttonText}>Administrador</Text>
+        <TouchableOpacity style={AdminOptionsStyles.button} onPress={() => handleRoleSelection("admin")}>
+          <Text style={AdminOptionsStyles.buttonText}>Administrador</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={LoginStyles.button} onPress={() => handleRoleSelection("user")}>
-          <Text style={LoginStyles.buttonText}>Usuario</Text>
+        <TouchableOpacity style={AdminOptionsStyles.button} onPress={() => handleRoleSelection("user")}>
+          <Text style={AdminOptionsStyles.buttonText}>Usuario</Text>
         </TouchableOpacity>
       </View>
     </View>
