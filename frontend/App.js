f8bd48b406cc4e348be6efcf0f8536d18ccf8b56
Alonso Alé-Jara <alonsoignacioalejara@gmail.com>
2024-12-17 11:39:36 -0300

    fix: draw navigator

--- frontend/App.js
+++ frontend/App.js
@@ -1,21 +1,23 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
 import { View, StyleSheet } from 'react-native';
 
 import Screens from './src/screens/Screens.index';
 import Header from './src/components/Header';
 
-// Crear el Stack y Drawer Navigators
+const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       <View style={styles.container}>
+
         {/* Drawer Navigator */}
         <Drawer.Navigator
-          initialRouteName="SetupIzaje"  // Define la pantalla inicial
+          initialRouteName="Home"
           screenOptions={{
             drawerStyle: {
               backgroundColor: '#dd0000',
@@ -31,7 +33,32 @@ const App = () => {
             },
           }}
         >
-          {/* Pantalla SetupIzaje */}
+          {/* Pantalla Home */}
+          <Drawer.Screen 
+            name="Home" 
+            component={Screens.Home} 
+            options={{
+              headerShown: false,  // Home no tiene Header
+            }} 
+          />
+
+          {/* Pantalla Login */}
+          <Drawer.Screen 
+            name="Login" 
+            component={Screens.Login} 
+            options={{
+              headerShown: false,  // Login no tiene Header
+            }} 
+          />
+
+          {/* Otras pantallas con Header */}
+          <Drawer.Screen 
+            name="AdminOptions" 
+            component={Screens.AdminOptions}
+            options={{
+              header: () => <Header />  // Usa el Header con Drawer para AdminOptions
+            }} 
+          />
           <Drawer.Screen 
             name="SetupIzaje" 
             component={Screens.SetupIzaje}
@@ -39,8 +66,13 @@ const App = () => {
               header: () => <Header />  // Usa el Header con Drawer para SetupIzaje
             }} 
           />
-          
-          {/* Pantalla Tablas */}
+          <Drawer.Screen 
+            name="PlanIzaje" 
+            component={Screens.PlanIzaje}
+            options={{
+              header: () => <Header />  // Usa el Header con Drawer para PlanIzaje
+            }} 
+          />
           <Drawer.Screen 
             name="Tablas" 
             component={Screens.Tablas}
@@ -48,8 +80,6 @@ const App = () => {
               header: () => <Header />  // Usa el Header con Drawer para Tablas
             }} 
           />
-          
-          {/* Pantalla GruaIzaje */}
           <Drawer.Screen 
             name="GruaIzaje" 
             component={Screens.GruaIzaje}
