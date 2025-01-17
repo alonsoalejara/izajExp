e45d9feecc0a140ae21e2984704946a1da0ffacd
Alonso Alé-Jara <alonsoignacioalejara@gmail.com>
2025-01-17 12:17:55 -0300

    chore: admin modal index

--- frontend/src/components/modals/ModalAdmin.index.js
+++ frontend/src/components/modals/ModalAdmin.index.js
@@ -1,10 +1,8 @@
-import ModalAddCrane from "./ModalAddCrane";
 import ModalEditCollab from "./ModalEditCollab";
 import ModalEditCrane from "./ModalEditCrane";
 import ModalEditSetupIzaje from "./ModalEditSetupIzaje";
 
 const ModalsAdmin = {
-    ModalAddCrane,
     ModalEditCollab,
     ModalEditCrane,
     ModalEditCollab,
