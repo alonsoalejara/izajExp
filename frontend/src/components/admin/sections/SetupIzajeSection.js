d4a9a6fdcfbcce05ea5aabf61392e280f4b607b3
Alonso Alé-Jara <alonsoignacioalejara@gmail.com>
2025-03-15 15:36:45 -0300

    feat: collab profile with plan

--- frontend/src/components/admin/sections/SetupIzajeSection.js
+++ frontend/src/components/admin/sections/SetupIzajeSection.js
@@ -4,181 +4,123 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import styles from '../../../styles/AdminSectionStyles';
 import getApiUrl from '../../../utils/apiUrl';
 import Components from '../../../components/Components.index';
-import { generarPDF } from '../../../utils/PDF/PDFGenerator'; 
 
-const SetupIzajeSection = ({ setupIzaje = [], setSetups, currentUser, isAdminPanel }) => {
-    const [selectedSetup, setSelectedSetup] = useState(null);
-    const filteredSetups = currentUser
+const SetupIzajeSection = ({ setupIzaje = [], setSetups, currentUser, isAdminPanel, buttonContainerStyle }) => {
+  const [selectedSetup, setSelectedSetup] = useState(null);
+  const filteredSetups = currentUser
     ? setupIzaje.filter(setup => setup.usuario._id === currentUser._id)
     : setupIzaje;
 
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    return `${day}-${month}-${year} a las ${hours}:${minutes} hrs`;
+  };
 
-    const formatDate = (dateString) => {
-        const date = new Date(dateString);
-        const day = String(date.getDate()).padStart(2, '0');
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const year = date.getFullYear();
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
-        
-        return `${day}-${month}-${year} a las ${hours}:${minutes} hrs`;
-    };
-
-    const confirmDelete = (_id) => {
-        Alert.alert(
-            'Confirmar Eliminación',
-            '¿Estás seguro de que deseas eliminar este plan de izaje?',
-            [
-                { text: 'Cancelar', style: 'cancel' },
-                { text: 'Confirmar', onPress: () => handleDelete(_id) },
-            ]
-        );
-    };
-
-    const handleDelete = async (_id) => {
-        try {
-            const accessToken = await AsyncStorage.getItem('accessToken');
-            if (!accessToken) {
-                alert('No autorizado. Por favor, inicie sesión nuevamente.');
-                return;
-            }
-            const response = await fetch(getApiUrl(`setupIzaje/${_id}`), {
-                method: 'DELETE',
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                },
-            });
-
-            if (response.ok) {
-                alert('Plan de izaje eliminado con éxito');
-                const updatedSetups = setupIzaje.filter((setup) => setup._id !== _id);
-                setSetups(updatedSetups);
-            } else {
-                alert('Error al eliminar el plan de izaje');
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
-
-    const handleSharePdf = (_id) => {
-        const setup = setupIzaje.find(item => item._id === _id);
-    
-        if (setup) {
-            const rows = setup.aparejos || [];
-            const totalPesoAparejos = rows.reduce((acc, aparejo) => acc + (aparejo.pesoUnitario * aparejo.cantidad), 0);
-            
-            const pesoTotalCarga = totalPesoAparejos + (setup.cargas?.pesoEquipo || 0) + (setup.cargas?.pesoGancho || 0);
-    
-            const cargaRows = [
-                { item: '1', descripcion: 'PESO DEL EQUIPO', valor: setup.cargas?.pesoEquipo ? `${setup.cargas.pesoEquipo} kg` : 'No disponible' },
-                { item: '2', descripcion: 'PESO DE APAREJOS', valor: totalPesoAparejos ? `${totalPesoAparejos} kg` : 'No disponible' },
-                { item: '3', descripcion: 'PESO GANCHO', valor: setup.cargas?.pesoGancho ? `${setup.cargas.pesoGancho} kg` : 'No disponible' },
-                { item: '4', descripcion: 'PESO TOTAL', valor: pesoTotalCarga ? `${pesoTotalCarga} kg` : 'No disponible' },
-                { item: '5', descripcion: 'RADIO DE TRABAJO MÁXIMO', valor: setup.cargas?.radioTrabajoMax ? `${setup.cargas.radioTrabajoMax} mts` : 'No disponible' },
-                { item: '6', descripcion: 'CAPACIDAD DE LEVANTE', valor: setup.cargas?.capacidadLevante ? `${setup.cargas.capacidadLevante} kg` : 'No disponible' },
-                { item: '7', descripcion: '% DE UTILIZACIÓN', valor: 'No disponible' },
-            ];                    
-    
-            const datosGruaRows = [
-                { item: '1', descripcion: 'Largo de Pluma', valor: setup.datos?.largoPluma ? `${setup.datos.largoPluma} m` : 'No disponible' },
-                { item: '2', descripcion: 'Contrapeso', valor: setup.datos?.contrapeso ? `${setup.datos.contrapeso} m` : 'No disponible' },
-            ];
-    
-            const aparecerosWithItem = rows.map((aparejo, index) => {
-                const pesoTotal = (typeof aparejo.pesoUnitario === 'number' && typeof aparejo.cantidad === 'number')
-                    ? aparejo.pesoUnitario * aparejo.cantidad
-                    : 'No disponible';
-                
-                console.log(`Aparejo ${index + 1}:`, aparejo, 'pesoTotal:', pesoTotal);
-    
-                return {
-                    item: (index + 1).toString(),
-                    descripcion: aparejo.descripcion || 'No disponible',
-                    cantidad: aparejo.cantidad ?? 'No disponible',
-                    pesoUnitario: aparejo.pesoUnitario ?? 'No disponible',
-                    valor: pesoTotal !== 'No disponible' ? `${pesoTotal} kg` : 'No disponible',
-                };
-            });
-    
-            console.log('SetupIzajeSection.js: aparecerosWithItem:', aparecerosWithItem);
-    
-            generarPDF(setup, aparecerosWithItem, totalPesoAparejos, cargaRows, datosGruaRows);
-        }
-    };    
-
-    return (
-        <View style={styles.section}>
-            {(filteredSetups && Array.isArray(filteredSetups) && filteredSetups.length > 0) ? (
-                filteredSetups.map((setup) => (
-                    <View key={setup._id} style={styles.card}>
-                        <TouchableOpacity onPress={() => setSelectedSetup(selectedSetup === setup._id ? null : setup._id)}>
-                            <Text style={[styles.cardTitle, { fontWeight: '700' }]}>
-                                Supervisor: <Text style={{ fontWeight: '400' }}>
-                                    {setup.usuario.nombre && setup.usuario.apellido
-                                        ? `${setup.usuario.nombre} ${setup.usuario.apellido}`
-                                        : 'No disponible'}
-                                </Text>
-                            </Text>
-                            <Text style={[styles.cardDetail, { fontWeight: '700', color: '#777' }]}>
-                                Especialidad: <Text style={{ fontWeight: '400' }}>
-                                    {setup.usuario.specialty || 'No disponible'}
-                                </Text>
-                            </Text>
-                            <View>
-                                <Text style={[styles.labelText, { top: 8, fontWeight: 'bold', fontSize: 16, color: '#777' }]}>
-                                    Fecha: <Text style={{ fontWeight: '400' }}>
-                                        {setup.createdAt ? formatDate(setup.createdAt) : 'No disponible'}
-                                    </Text>
-                                </Text>
-                            </View>
-                        </TouchableOpacity>
-                        {selectedSetup === setup._id && (
-                            <View style={styles.cardExpandedDetails}>
-
-                                {/* Botones para compartir PDF y eliminar */}
-                                <View style={[
-                                    styles.buttonContainerCard, 
-                                    { 
-                                        marginLeft: -110, 
-                                        marginTop: isAdminPanel ? -3 : 2, 
-                                        marginBottom: isAdminPanel ? -160 : -100,
-                                        bottom: 0, 
-                                        top: 15, 
-                                        left: -5 
-                                    }
-                                    ]}>
-                                    <Components.Button
-                                        label="Ver"
-                                        onPress={() => console.log('Ver presionado')}
-                                        isCancel={true}
-                                        style={[styles.button, { backgroundColor: 'transparent', width: '0%', height: '62%', marginHorizontal: -53 }]}
-                                    />
-                                    <Components.Button
-                                        label="Editar"
-                                        onPress={() => console.log('Editar presionado')}
-                                        isCancel={true}
-                                        style={[styles.button, { backgroundColor: 'transparent', width: '0%', height: '62%', marginHorizontal: -53 }]}
-                                    />
-                                    {isAdminPanel && (
-                                        <Components.Button
-                                        label="Eliminar"
-                                        onPress={() => console.log('Eliminar presionado')}
-                                        isCancel={true}
-                                        style={[styles.button, { backgroundColor: 'transparent', width: '0%', height: '62%', marginHorizontal: -53 }]}
-                                        />
-                                    )}
-                                    </View>
-                            </View>
-                        )}
-                    </View>
-                ))
-            ) : (
-                <Text>No hay planes de izaje disponibles.</Text>
-            )}
-        </View>
+  const confirmDelete = (_id) => {
+    Alert.alert(
+      'Confirmar Eliminación',
+      '¿Estás seguro de que deseas eliminar este plan de izaje?',
+      [
+        { text: 'Cancelar', style: 'cancel' },
+        { text: 'Confirmar', onPress: () => handleDelete(_id) },
+      ]
     );
+  };
+
+  const handleDelete = async (_id) => {
+    try {
+      const accessToken = await AsyncStorage.getItem('accessToken');
+      if (!accessToken) {
+        alert('No autorizado. Por favor, inicie sesión nuevamente.');
+        return;
+      }
+      const response = await fetch(getApiUrl(`setupIzaje/${_id}`), {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      if (response.ok) {
+        alert('Plan de izaje eliminado con éxito');
+        const updatedSetups = setupIzaje.filter((setup) => setup._id !== _id);
+        setSetups(updatedSetups);
+      } else {
+        alert('Error al eliminar el plan de izaje');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <View style={styles.section}>
+      {(filteredSetups && Array.isArray(filteredSetups) && filteredSetups.length > 0) ? (
+        filteredSetups.map((setup) => (
+          <View key={setup._id} style={styles.card}>
+            <TouchableOpacity onPress={() => setSelectedSetup(selectedSetup === setup._id ? null : setup._id)}>
+              <Text style={[styles.cardTitle, { fontWeight: '700' }]}>
+                Supervisor: <Text style={{ fontWeight: '400' }}>
+                  {setup.usuario.nombre && setup.usuario.apellido
+                    ? `${setup.usuario.nombre} ${setup.usuario.apellido}`
+                    : 'No disponible'}
+                </Text>
+              </Text>
+              <Text style={[styles.cardDetail, { fontWeight: '700', color: '#777' }]}>
+                Especialidad: <Text style={{ fontWeight: '400' }}>
+                  {setup.usuario.specialty || 'No disponible'}
+                </Text>
+              </Text>
+              <View>
+                <Text style={[styles.labelText, { top: 8, fontWeight: 'bold', fontSize: 16, color: '#777' }]}>
+                  Fecha: <Text style={{ fontWeight: '400' }}>
+                    {setup.createdAt ? formatDate(setup.createdAt) : 'No disponible'}
+                  </Text>
+                </Text>
+              </View>
+            </TouchableOpacity>
+            {selectedSetup === setup._id && (
+              <View style={styles.cardExpandedDetails}>
+                <View style={[
+                  styles.buttonContainerCard,
+                  buttonContainerStyle
+                    ? buttonContainerStyle
+                    : { marginLeft: -110, marginTop: isAdminPanel ? -3 : 2, marginBottom: isAdminPanel ? -160 : -100, bottom: 0, top: 15, left: -5 }
+                ]}>
+                  <Components.Button
+                    label="Ver"
+                    onPress={() => console.log('Ver presionado')}
+                    isCancel={true}
+                    style={[styles.button, { backgroundColor: 'transparent', width: '0%', height: '62%', marginHorizontal: -53 }]}
+                  />
+                  <Components.Button
+                    label="Editar"
+                    onPress={() => console.log('Editar presionado')}
+                    isCancel={true}
+                    style={[styles.button, { backgroundColor: 'transparent', width: '0%', height: '62%', marginHorizontal: -53 }]}
+                  />
+                  {isAdminPanel && (
+                    <Components.Button
+                      label="Eliminar"
+                      onPress={() => console.log('Eliminar presionado')}
+                      isCancel={true}
+                      style={[styles.button, { backgroundColor: 'transparent', width: '0%', height: '62%', marginHorizontal: -53 }]}
+                    />
+                  )}
+                </View>
+              </View>
+            )}
+          </View>
+        ))
+      ) : (
+        <Text>No hay planes de izaje que mostrar.</Text>
+      )}
+    </View>
+  );
 };
-    
 
 export default SetupIzajeSection;
--- frontend/src/screens/CollabProfile.js
+++ frontend/src/screens/CollabProfile.js
@@ -1,50 +1,40 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { View, Text, Image, ImageBackground, TouchableOpacity, Animated, ScrollView } from 'react-native';
-import { useNavigation, useRoute } from '@react-navigation/native';
 import Svg, { LinearGradient, Stop, Rect } from 'react-native-svg';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import getApiUrl from '../utils/apiUrl';
-import Components from '../components/Components.index';
-import Section from '../components/admin/sections/Section.index';
 import styles from '../styles/ProfileStyles';
-import IconFA from 'react-native-vector-icons/FontAwesome';
+import Section from '../components/admin/sections/Section.index';
+import getApiUrl from '../utils/apiUrl';
 
 const CollabProfile = () => {
   const navigation = useNavigation();
   const route = useRoute();
-  const { userData } = route.params;
-  const [user, setUser] = useState(userData);
-  console.log('Datos en estado "user" en CollabProfile:', user);
-  const [selectedButton, setSelectedButton] = useState('Datos');
+  const { userData } = route.params || {};
+
+  // Estado para los planes de izaje
   const [setupIzaje, setSetups] = useState([]);
+  // Estado para la pestaña seleccionada: 'Datos' o 'Planes'
+  const [selectedButton, setSelectedButton] = useState('Datos');
+  const animations = useRef({
+    Datos: new Animated.Value(1), // por defecto se muestra esta pestaña
+    Planes: new Animated.Value(0),
+  });
 
-  useEffect(() => {
-    if (userData) {
-      setUser(userData);
-    }
-  }, [userData]); 
-
-  useEffect(() => {
-    if (route.params?.userData) {
-      console.log('Cambio en userData:', route.params.userData);
-      setUser(route.params.userData);
-    }
-  }, [route.params?.userData]);
-
+  // Obtención de todos los planes de izaje
   useEffect(() => {
     const fetchSetups = async () => {
       try {
         const token = await AsyncStorage.getItem('accessToken');
-        if (!token || !userData) return;
-
-        const response = await fetch(getApiUrl(`setupIzaje?userId=${userData._id}`), {
+        if (!token) return;
+        const response = await fetch(getApiUrl('setupIzaje'), {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${token}`,
           },
         });
-
         const data = await response.json();
         if (data && data.data) {
           setSetups(data.data);
@@ -55,16 +45,12 @@ const CollabProfile = () => {
     };
 
     fetchSetups();
-  }, [userData]);
+  }, []);
 
-  const animations = useRef({
-    Datos: new Animated.Value(0),
-    Plan: new Animated.Value(0),
-  });
-
+  // Función para cambiar de pestaña con animación
   const handlePressButton = (button) => {
     setSelectedButton(button);
-    const otherButton = button === 'Datos' ? 'Plan' : 'Datos';
+    const otherButton = button === 'Datos' ? 'Planes' : 'Datos';
     Animated.timing(animations.current[otherButton], {
       toValue: 0,
       duration: 300,
@@ -79,8 +65,7 @@ const CollabProfile = () => {
 
   return (
     <View style={styles.container}>
-
-
+      {/* Cabecera con imagen, gradiente y título con ícono para regresar */}
       <View style={styles.circleContainer}>
         <ImageBackground
           source={require('../../assets/grua-home.png')}
@@ -94,22 +79,35 @@ const CollabProfile = () => {
             </LinearGradient>
             <Rect width="100%" height="100%" fill="url(#grad1)" />
           </Svg>
-          <Text style={[styles.title, { bottom: '17%' }]}>Perfil</Text>
-            {/* Botón de regresar con FontAwesome */}
-            <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
-                <IconFA name="angle-left" size={40} color="white" top="100" right="150"/>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              position: 'absolute',
+              top: 455,
+              left: 272,
+              right: 0,
+            }}
+          >
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Icon name="keyboard-arrow-left" size={40} color="#fff" style={{ right: 150, bottom: 72 }} />
             </TouchableOpacity>
+            <Text style={[styles.title, { flex: 1, textAlign: 'center', fontSize: 26 }]}>
+              Perfil
+            </Text>
+          </View>
         </ImageBackground>
       </View>
 
+      {/* Imagen de perfil */}
       <Image
-        source={user?.profileImage || require('../../assets/blank-user-image.png')}
-        style={styles.profileImage}
-        top="170"
+        source={userData?.profileImage || require('../../assets/blank-user-image.png')}
+        style={[styles.profileImage, { top: 186 }]}
       />
 
-      <View style={styles.userButtonsContainer}>
-        {['Datos', 'Plan'].map((section) => (
+      {/* Botones de pestañas */}
+      <View style={[styles.userButtonsContainer, { top: 320 }]}>
+        {['Datos', 'Planes'].map((section) => (
           <TouchableOpacity
             key={section}
             style={[styles.userButton, selectedButton === section && { color: 'red' }]}
@@ -133,18 +131,54 @@ const CollabProfile = () => {
         ))}
       </View>
 
+      {/* Contenido según la pestaña seleccionada */}
       {selectedButton === 'Datos' && (
-        <View style={[styles.userDataContainer, { top: -20 }]}>
-          <Text style={styles.userName}>{user?.nombre || user?.name}</Text>
-          <Text style={styles.userEmail}>{user?.email}</Text>
-          <Components.UserDataSection user={user} />
+        <View style={[styles.roundedInputContainer, { marginTop: 330, paddingHorizontal: 20 }]}>
+          <View style={styles.infoContainer}>
+            <Icon name="person" size={24} color="#ee0000" style={styles.icon} />
+            <Text style={styles.value}>
+              {userData?.nombre} {userData?.apellido}
+            </Text>
+          </View>
+          <View style={styles.infoContainer}>
+            <Icon name="fingerprint" size={24} color="#ee0000" style={styles.icon} />
+            <Text style={styles.value}>{userData?.rut}</Text>
+          </View>
+          <View style={styles.infoContainer}>
+            <Icon name="phone" size={24} color="#ee0000" style={styles.icon} />
+            <Text style={styles.value}>{userData?.phone}</Text>
+          </View>
+          <View style={styles.infoContainer}>
+            <Icon name="work" size={24} color="#ee0000" style={styles.icon} />
+            <Text style={styles.value}>{userData?.specialty}</Text>
+          </View>
+          <View style={styles.infoContainer}>
+            <Icon name="email" size={24} color="#ee0000" style={styles.icon} />
+            <Text style={styles.value}>{userData?.email}</Text>
+          </View>
         </View>
       )}
 
-      {selectedButton === 'Plan' && (
-        <View style={{ top: 300, flex: 1 }}>
-          <ScrollView contentContainerStyle={{ paddingBottom: 310 }}>
-            <Section.SetupIzajeSection setupIzaje={setupIzaje} setSetups={setSetups} />
+      {selectedButton === 'Planes' && (
+        <View style={{ top: 320, flex: 1 }}>
+          <ScrollView contentContainerStyle={{ paddingBottom: 450, marginBottom: 20 }}>
+            {/* Aquí se pasan:
+                - setupIzaje y setSetups (planes de izaje)
+                - currentUser con el usuario del perfil para filtrar los setups
+                - Un estilo personalizado para el contenedor de botones */}
+            <Section.SetupIzajeSection
+              setupIzaje={setupIzaje}
+              setSetups={setSetups}
+              currentUser={userData}
+              buttonContainerStyle={{
+                marginLeft: -110,
+                marginTop: 2,
+                marginBottom: -25,
+                bottom: 0,
+                top: 15,
+                left: -5,
+              }}
+            />
           </ScrollView>
         </View>
       )}
